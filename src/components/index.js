0bac930957b05ebf1a260e801181e99e401ed77a
Mike Riccelli <mriccelli@ons.org>
2023-09-26 14:04:17 -0400

    react set up and nav, hero and status set up and styling

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Navbar from "./NavBar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./footer";
+import Testimonials from "./testimonials";
+import Hero from "./hero";
+import GetStarted from "./GetStarted";
+
+export {
+  Navbar,
+  GetStarted,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
